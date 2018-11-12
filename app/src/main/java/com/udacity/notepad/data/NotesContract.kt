f24da43db1dbdd926d900b43d4ea4b166bdf8b9f
Guillem Roca <guillemroc@gmail.com>
2018-11-12 01:46:03 +0100

    Format correctly all classes

--- app/src/main/java/com/udacity/notepad/data/NotesContract.kt
+++ app/src/main/java/com/udacity/notepad/data/NotesContract.kt
@@ -10,12 +10,12 @@ import com.udacity.notepad.data.NotesContract.NoteTable._TABLE_NAME
 object NotesContract {
 
   object NoteTable {
-      val _ID = "_id"
-      val _TABLE_NAME = "notes"
-      val TEXT = "text"
-      val IS_PINNED = "is_pinned"
-      val CREATED_AT = "created_at"
-      val UPDATED_AT = "updated_at"
+    val _ID = "_id"
+    val _TABLE_NAME = "notes"
+    val TEXT = "text"
+    val IS_PINNED = "is_pinned"
+    val CREATED_AT = "created_at"
+    val UPDATED_AT = "updated_at"
   }
 
   val SQL_CREATE_ENTRIES = """"CREATE TABLE $_TABLE_NAME (
@@ -24,7 +24,7 @@ object NotesContract {
       $IS_PINNED INTEGER,
       $CREATED_AT INTEGER,
       $UPDATED_AT INTEGER
-  )"""
+  )""".trimMargin()
 
   val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS $_TABLE_NAME"
 
